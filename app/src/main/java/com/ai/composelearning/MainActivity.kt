2a8fa61bb8178ef4bac968c5dfc07c77f42f1967
zeshansahi4u <zeshansahi4u@gmail.com>
2024-05-29 09:03:53 +0500

    textfiled and OutLinedText done

--- app/src/main/java/com/ai/composelearning/MainActivity.kt
+++ app/src/main/java/com/ai/composelearning/MainActivity.kt
@@ -7,7 +7,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,15 +37,44 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainContent() {
     ComposelearningTheme {
-        Column(modifier = Modifier.fillMaxSize().background(Color.LightGray).padding(vertical = 10.dp)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray)
+                .padding(vertical = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-            ExpandableCard()
+            Greating()
+            OutLinedText()
         }
     }
 }
 
-
-
+@Composable
+fun Greating() {
+    var text by remember { mutableStateOf("") }
+    TextField(
+        value = text,
+        onValueChange = { newString ->
+            text = newString
+        },
+        label = { Text("Enter your name") },
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "search") },
+        trailingIcon = { Icon(imageVector = Icons.Default.Check, contentDescription = "search") })
+}
+@Composable
+fun OutLinedText() {
+    var text by remember { mutableStateOf("") }
+    OutlinedTextField(
+        value = text,
+        onValueChange = { newString ->
+            text = newString
+        },
+        label = { Text("Enter your name") },
+        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "search") },
+        trailingIcon = { Icon(imageVector = Icons.Default.Check, contentDescription = "search") })
+}
 
 
 @Preview(showBackground = true)
