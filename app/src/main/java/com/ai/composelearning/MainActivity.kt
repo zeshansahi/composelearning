77ece40b5d97c9e5542934fea20c1e81fe823545
zeshansahi4u <zeshansahi4u@gmail.com>
2024-05-29 13:14:02 +0500

    google signinbutton done

--- app/src/main/java/com/ai/composelearning/MainActivity.kt
+++ app/src/main/java/com/ai/composelearning/MainActivity.kt
@@ -4,22 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.integerResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,42 +55,13 @@ fun MainContent() {
                 .padding(vertical = 10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            GoogleLoginButton()
 
-            Greating()
-            OutLinedText()
         }
     }
 }
 
-@Composable
-fun Greating() {
-    var text by remember { mutableStateOf("") }
-    TextField(
-        value = text,
-        onValueChange = { newString ->
-            text = newString
-        },
-        label = { Text("Enter your name") },
-        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "search") },
-        trailingIcon = { Icon(imageVector = Icons.Default.Check, contentDescription = "search") })
-}
 
-@Composable
-fun OutLinedText() {
-    var text by remember { mutableStateOf("") }
-    OutlinedTextField(
-        value = text,
-        onValueChange = { newString ->
-            text = newString
-        },
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Email,
-            imeAction = ImeAction.Search
-        ),
-        label = { Text("Enter your name") },
-        leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "search") },
-        trailingIcon = { Icon(imageVector = Icons.Default.Check, contentDescription = "search") })
-}
 
 
 @Preview(showBackground = true)
