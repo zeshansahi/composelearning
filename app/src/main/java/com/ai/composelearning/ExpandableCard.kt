4b5922496c4cded515140718aa24f897829ae592
zeshansahi4u <zeshansahi4u@gmail.com>
2024-05-29 08:45:37 +0500

    expandable

---
+++ app/src/main/java/com/ai/composelearning/ExpandableCard.kt
@@ -0,0 +1,99 @@
+package com.ai.composelearning
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.ContentAlpha
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ExpandableCard() {
+    var expandedState by remember { mutableStateOf(false) }
+    val rotationState by animateFloatAsState(
+        targetValue = if (expandedState) 180f else 0f
+    )
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+            ),
+        onClick = {
+            expandedState = !expandedState
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color.White)
+                .padding(12.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "My Title",
+                    color = colorResource(id = R.color.black),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.weight(6f),
+                    overflow = TextOverflow.Ellipsis
+                )
+                IconButton(
+                    modifier = Modifier
+                        .weight(1f)
+                        .alpha(ContentAlpha.medium)
+                        .rotate(rotationState),
+                    onClick = {
+                        expandedState = !expandedState
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Drop-Down Arrow"
+                    )
+                }
+
+            }
+
+            if (expandedState) {
+                Text(text = "This placeholder text".repeat(10))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExpandableCardPreview() {
+    ExpandableCard()
+}
--- app/src/main/java/com/ai/composelearning/MainActivity.kt
+++ app/src/main/java/com/ai/composelearning/MainActivity.kt
@@ -4,29 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.ai.composelearning.ui.theme.ComposelearningTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             MainContent()
+
         }
     }
 }
@@ -34,46 +29,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainContent() {
     ComposelearningTheme {
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize().background(Color.LightGray).padding(vertical = 10.dp)) {
 
-            TextCustomization2()
-
+            ExpandableCard()
         }
     }
 }
 
 
-@Composable
-fun TextCustomization2() {
 
-    Text(
-        buildAnnotatedString {
-
-            withStyle(
-                style = SpanStyle(
-                    color = Color.Red,
-                    fontSize = 30.sp,
-                    fontWeight = FontWeight.Bold
-                )
-            ) {
-                append("A")
-            }
-            withStyle(
-                style = SpanStyle(
-                    color = Color.Red,
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Bold,
-                    baselineShift = BaselineShift.Superscript
-                )
-            ) {
-                append("3")
-            }
-
-
-        },
-        modifier = Modifier.width(200.dp)
-    )
-}
 
 
 @Preview(showBackground = true)
